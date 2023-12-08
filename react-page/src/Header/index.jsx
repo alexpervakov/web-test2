dbdf59dcc38f2656a115ad43648b21def5e1ac84
Alexinerror <sarmat674@gmail.com>
2023-12-09 03:13:17 +0400

    changed code and css for hamburger menu

--- react-page/src/Header/index.jsx
+++ react-page/src/Header/index.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
-import logo from '../assets/disney_logo_dark@2x-45d70f7dd57b.png';
-import styles from './index.module.css';
+import React, { useState } from 'react';
+import './index.module.css';
+// import logo from '../assets/disney_logo_dark@2x-45d70f7dd57b.png';
 
-const Header = () => {
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <header className={styles.header}>
-      <img src={logo} alt="logo" />
-      <a href="disney">DISNEY+</a>
-      <a href="shop">SHOP</a>
-      <a href="movies">MOVIES</a>
-      <a href="shows">SHOWS</a>
-      <a href="parks">PARKS</a>
-      <a href="disney">DISNEY100</a>
-    </header>
-  )
-}
+    <div className="hamburger-menu">
+      <button className="hamburger-icon" onClick={toggleMenu}>
+        ☰
+      </button>
+      {isOpen && (
+        <div className="menu-items">
+          <a href="disney">DISNEY+</a>
+          <a href="shop">SHOP</a>
+          <a href="movies">MOVIES</a>
+          <a href="shows">SHOWS</a>
+          <a href="parks">PARKS</a>
+          <a href="disney">DISNEY100</a>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Header;
+export default HamburgerMenu;
+
+//   const Header = () => {
+//    return (
+//     <header className={styles.header}>
+//        <img src={logo} alt="logo" />
+//        <a href="disney">DISNEY+</a>
+//        <a href="shop">SHOP</a>
+//        <a href="movies">MOVIES</a>
+//        <a href="shows">SHOWS</a>
+//        <a href="parks">PARKS</a>
+//        <a href="disney">DISNEY100</a>
+//      </header>
+//    )
+//  }
+
+//  export default Header; 
