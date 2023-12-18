800362e8312141217744bd58d6fcc848f12cd07c
Alexinerror <sarmat674@gmail.com>
2023-12-19 00:38:09 +0400

    modified css for hamburger menu

--- react-page/src/Header/index.jsx
+++ react-page/src/Header/index.jsx
@@ -2,36 +2,27 @@ import React, { useState } from 'react';
 import './HamburgerMenu.css'
 // import logo from '../assets/disney_logo_dark@2x-45d70f7dd57b.png';
 
-function HamburgerMenu() {
+const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
+  const handleMenuClick = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
-      <button className="hamburger-icon" onClick={toggleMenu}>
-        <div className="line"></div>
-        <div className="line"></div>
-      </button>
-      {isOpen && (
-        <div className="menu-items">
-          <a href="disney">DISNEY+</a>
-          <div className='line1'></div>
-          <a href="shop">SHOP</a>
-          <div className='line1'></div>
-          <a href="movies">MOVIES</a>
-          <div className='line1'></div>
-          <a href="shows">SHOWS</a>
-          <div className='line1'></div>
-          <a href="parks">PARKS</a>
-          <div className='line1'></div>
-          <a href="disney">DISNEY100</a>
-        </div>
-      )}
+    <div className={`hamburger-menu ${isOpen ? 'open' : ''}`} onClick={handleMenuClick}>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="links">
+        <a href="disney">DISNEY+</a>
+        <a href="shop">SHOP</a>
+        <a href="movies">MOVIES</a>
+        <a href="shows">SHOWS</a>
+        <a href="parks">PARKS</a>
+      </div>
     </div>
   );
-}
+};
 
 export default HamburgerMenu;
 
